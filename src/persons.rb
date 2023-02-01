e7232fc261770327001fa8f119ab8d8a1a791d33
kennankole <kennankole@gmail.com>
2023-02-01 08:51:19 +0300

    Modified persons.rb

--- src/persons.rb
+++ src/persons.rb
@@ -10,14 +10,11 @@ class Person
   end
 
   def of_age?
-    return true if age&.>= 18
-
-    false
+    @age >= 18
   end
 
   def can_use_services?
     return true if @age >= 18 && @parent_permission == true
-
     false
   end
 end
