b600bdbe1804f3f68702879576a3951bb9b10664
kennankole <kennankole@gmail.com>
2023-02-02 13:41:37 +0300

    Fixing linter errors

--- src/run.rb
+++
@@ -1,8 +0,0 @@
-require "./src/class.rb"
-require "./src/student.rb"
-
-classroom = Classroom.new("Class 7")
-
-student = Student.new(classroom)
-puts student.classroom.label
-puts classroom.students.classroom
